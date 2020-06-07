105b7f3360131b0312ec50c5c2fe9af77f9c6ff6
mikamishogo <sho.mikami.7@gmail.com>
2020-06-07 11:42:26 +0900

    Initial Commit

---
+++ check2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  check2
+//
+//  Created by 三上翔吾 on 2020/06/07.
+//  Copyright © 2020 Mikami Sho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
